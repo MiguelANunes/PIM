3d12e70cdc5b87d7f9fd41a9e0eb3de3ccc176cd
Daniella Vasconcellos <daniellavasconc@gmail.com>
2022-10-24 03:29:52 -0300

    aaaaaaaaaaaaaa

--- Contraste/equalizer.py
+++ Contraste/equalizer.py
@@ -1,19 +1,18 @@
-import cv2, skimage
+import cv2
 from matplotlib import pyplot as plt
  
 imagem1 = cv2.imread('Imagens/Outono.png')
-pathOut = "D:/Documentos/Code/UDESC/PIM/Contraste/Resultados/"
+pathOut = "D:/Documentos/Code/UDESC/PIM/Contraste/Item3/"
 
 def histograma(img, nome):
     plt.hist(img.ravel(), 256, [0,256])
     plt.savefig(pathOut+"Histograma"+nome)
     plt.close()
 
-imagem_yuv = skimage.color.rgb2yiq(imagem1)
-ok = histograma(imagem_yuv, "OUTONO PRE.png")
+imagem_yuv = cv2.cvtColor(imagem1,cv2.COLOR_BGR2YUV)
+histograma(imagem_yuv, "RGB_PRA_YUV.png")
 imagem_yuv[:,:,0] = cv2.equalizeHist(imagem_yuv[:,:,0])
-img_equalizada = skimage.color.yiq2rgb(imagem_yuv)
-iguess = histograma(img_equalizada, "OUTONO POS.png")
-print(iguess)
+img_equalizada = cv2.cvtColor(imagem_yuv, cv2.COLOR_YUV2BGR)
+plt.hist1(img_equalizada.ravel(), 256, [0,256])
 
 cv2.imwrite('{imagem1}.jpg', img_equalizada)
