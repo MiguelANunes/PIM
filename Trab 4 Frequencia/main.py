ea29ed77e80eddc9cb71477e4910d11613b6464f
Daniella Vasconcellos <daniellavasconc@gmail.com>
2022-10-31 01:04:16 -0300

    resolvendo frequencia

--- Trab 4 Frequencia/main.py
+++ Trab 4 Frequencia/main.py
@@ -1,20 +1,26 @@
-import cv2, skimage, math, numpy
+import cv2
+import skimage
+import math
+import numpy
 from matplotlib import pyplot as plt
 from skimage.io import imread, imshow
 from copy import copy, deepcopy
 
-pathIn  = "/home/miguel/Documents/Matérias/PIM/Trab 4 Frequencia/Imagens/"
-pathOut = "/home/miguel/Documents/Matérias/PIM/Trab 4 Frequencia/Resultados/"
+pathIn = "Imagens/"
+pathOut = "Resultados/"
+
 
 def fourier_mascara_horizontal(img, mascara):
     img[235:240, :230] = mascara
-    img[235:240,-230:] = mascara
+    img[235:240, -230:] = mascara
     return img
+
 
 def fourier_mascara_vertical(img, mascara):
     img[:225, 235:240] = mascara
-    img[-225:,235:240] = mascara
+    img[-225:, 235:240] = mascara
     return img
+
 
 def fourier(mascara):
     imagens = ["Folhas.jpg"]
@@ -22,18 +28,21 @@ def fourier(mascara):
     for imagem in imagens:
         img = imread(pathIn+imagem)
 
-        imgFourier    = numpy.fft.fftshift(numpy.fft.fft2(img))
+        imgFourier = numpy.fft.fftshift(numpy.fft.fft2(img))
         # imgFourierHor = fourier_mascara_horizontal(copy(imgFourier), mascara)
         # imgFourierVer = fourier_mascara_vertical(copy(imgFourier), mascara)
         # imgFourierHorVer = fourier_mascara_horizontal(fourier_mascara_vertical(copy(imgFourier), mascara), mascara)
 
-        imgTransformada = abs(numpy.fft.ifft2(fourier_mascara_horizontal(fourier_mascara_vertical(imgFourier, mascara), mascara))) 
+        imgTransformada = abs(numpy.fft.ifft2(fourier_mascara_horizontal(
+            fourier_mascara_vertical(imgFourier, mascara), mascara)))
         plt.figure(num=None, figsize=(8, 6), dpi=80)
-        plt.imshow(imgTransformada, cmap='gray');
+        plt.imshow(imgTransformada, cmap='gray')
         plt.show()
+
 
 def main():
     fourier(0)
+
 
 if __name__ == "__main__":
     main()
---
+++ Trab 4 Frequencia/tutorialprof.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+dark_image = cv2.imread("Imagens/Folhas.jpg", 0)
+
+dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(dark_image))
+plt.figure(num=None, figsize=(8, 6), dpi=80)
+plt.imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+
+
+def fourier_masker_ver(image, i):
+    f_size = 15
+    dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(image))
+    dark_image_grey_fourier[:225, 235:240] = i
+    dark_image_grey_fourier[-225:, 235:240] = i
+    fig, ax = plt.subplots(1, 2, figsize=(15, 15))
+    ax[0].imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+    ax[0].set_title('Masked Fourier', fontsize=f_size)
+    ax[1].imshow(abs(np.fft.ifft2(dark_image_grey_fourier)),
+                 cmap='gray')
+    ax[1].set_title('Transformed Greyscale Image',
+                    fontsize=f_size)
+
+
+def fourier_masker_hor(image, i):
+    f_size = 15
+    dark_image_grey_fourier = np.fft.fftshift(np.fft.fft2(image))
+    dark_image_grey_fourier[235:240, :230] = i
+    dark_image_grey_fourier[235:240, -230:] = i
+    fig, ax = plt.subplots(1, 2, figsize=(15, 15))
+    ax[0].imshow(np.log(abs(dark_image_grey_fourier)), cmap='gray')
+    ax[0].set_title('Masked Fourier', fontsize=f_size)
+    ax[1].imshow(abs(np.fft.ifft2(dark_image_grey_fourier)),
+                 cmap='gray')
+    ax[1].set_title('Transformed Greyscale Image',
+                    fontsize=f_size)
+
+
+def fourier_iterator(image, value_list):
+    for i in value_list:
+        img_ver = fourier_masker_ver(image, i)
+        img_hor = fourier_masker_hor(img_ver, i)
+
+    return img_hor
+
+
+fourier_masker_ver(dark_image, 1)
+fourier_masker_hor(dark_image, 1)
+fourier_iterator(dark_image, [0, 1, 100])
+plt.show()
